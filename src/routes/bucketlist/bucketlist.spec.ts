db84fd775402e7b8e7d6411349232a10eeab91bc
Micah Akpan <mickey4sure2k7@gmail.com>
2019-09-21 22:18:33 +0100

    feat:build view single bucketlist route

--- src/routes/bucketlist/bucketlist.spec.ts
+++ src/routes/bucketlist/bucketlist.spec.ts
@@ -5,12 +5,21 @@ describe('Bucketlists API test', () => {
   let request;
   beforeAll(() => {
     request = supertest(app);
-  })
+  });
   describe('Fetch all bucketlists', () => {
     it('should return all bucketlists', async () => {
       const response = await request.get('/api/v1/bucketlists');
       expect(Array.isArray(response.body.data)).toEqual(true);
       expect(response.body.status).toEqual(200);
-    })
-  })
-})
+    });
+  });
+
+  describe('Fetch a single bucketlist', () => {
+    it('should return a single bucketlist', async () => {
+      const response = await request.get('/api/v1/bucketlists/1');
+      expect(Array.isArray(response.body.data)).toEqual(true);
+      expect(response.body.data[0]).toBe(null);
+      expect(response.body.status).toEqual(200);
+    });
+  });
+});
--- src/services/BucketlistService.ts
+++ src/services/BucketlistService.ts
@@ -4,6 +4,10 @@ class BucketlistService {
   static getAllBucketlists() {
     return Bucketlist.findAll();
   }
+
+  static getABucketlist(id: string) {
+    return Bucketlist.findByPk(id);
+  }
 }
 
 export default BucketlistService;
